e8fccac86d695282cbfca38e1a3d30ee46f63986
Aiyane <2310091880@qq.com>
2018-02-05 06:29:11 +0800

    添加测试

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from application import Jay
+from application import Jay, render_template
 
 app = Jay()
 
@@ -9,6 +9,20 @@ app = Jay()
 @app.route('/')
 def index():
     return 'Hello!'
+
+
+@app.route('/students/')
+def student():
+    people1 = {"name": "张三", "age": "18", "major": "计算机"}
+    people2 = {"name": "李四", "age": "19", "major": "金融"}
+    people3 = {"name": "王五", "age": "20", "major": "法律"}
+
+    many_people = [
+        people1,
+        people2,
+        people3
+    ]
+    return render_template("model.html", many_people=many_people)
 
 
 @app.route('/hello')
